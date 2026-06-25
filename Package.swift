b48c0c3b00e411d557dc5c2a138bb40ddca115d5
Egor Churikov <churikov.egor@bk.ru>
2026-06-25 22:08:25 +0300

    fix: add v prefix to download URL in Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ var binaryTarget: Target = .binaryTarget(
 // CI will replace the nils with the actual values when building a release
 let version: String = "0.5.2"
 let binaryURL: String =
-    "https://github.com/Churikov0112/valhalla-mobile/releases/download/\(version)/valhalla-wrapper.xcframework.zip"
+    "https://github.com/Churikov0112/valhalla-mobile/releases/download/v\(version)/valhalla-wrapper.xcframework.zip"
 let binaryChecksum: String = "f36fd992542bf200d638a762363f58d675397ad9043a7d65f91e3a254b899dee"
 
 if !useLocalBinary {
