b9df9188cf5f7aa5ea8312f0bf6124387a6dc596
Egor Churikov <churikov.egor@bk.ru>
2026-06-25 22:15:53 +0300

    chore: bump to v0.5.3

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ var binaryTarget: Target = .binaryTarget(
 )
 
 // CI will replace the nils with the actual values when building a release
-let version: String = "0.5.2"
+let version: String = "0.5.3"
 let binaryURL: String =
     "https://github.com/Churikov0112/valhalla-mobile/releases/download/v\(version)/valhalla-wrapper.xcframework.zip"
 let binaryChecksum: String = "f36fd992542bf200d638a762363f58d675397ad9043a7d65f91e3a254b899dee"
